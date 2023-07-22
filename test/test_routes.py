454cc119c03438d5c2caaf1665cbb741c2e8036d
Pradnyanandana Suwitra <pradnyanandana@gmail.com>
2023-07-23 00:40:11 +0800

    Add service test

--- test/test_routes.py
+++ test/test_routes.py
@@ -1,40 +1,49 @@
-import pytest
-from controller import app
+# import app
+# import pytest
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
+# @pytest.fixture
+# def client():
+#     with app.test_client() as client:
+#         yield client
 
-def test_save_emails(client):
-    data = {
-        "event_id": 1,
-        "email_subject": "Test Subject",
-        "email_content": "Test Content",
-        "timestamp": "2023-07-31 15:30:00"
-    }
+# def test_save_emails(client):
+#     data = {
+#         "event_id": 123,
+#         "email_subject": "Test Subject",
+#         "email_content": "Test Content",
+#         "timestamp": "2023-07-31 15:30:00",
+#         "recipients": ["recipient1@example.com", "recipient2@example.com"]
+#     }
 
-    response = client.post('/save_emails', json=data)
-    assert response.status_code == 201
-    assert 'message' in response.json
-    assert 'data' in response.json
+#     response = client.post('/save_emails', json=data)
+#     assert response.status_code == 201
+#     assert 'message' in response.json
+#     assert 'data' in response.json
 
-def test_get_all_emails(client):
-    response = client.get('/emails')
-    assert response.status_code == 200
-    assert 'emails' in response.json
-    assert 'pagination' in response.json
+# def test_get_all_emails(client):
+#     response = client.get('/save_emails')
+#     assert response.status_code == 200
+#     assert 'data' in response.json
+#     assert 'pagination' in response.json
 
-def test_get_email_by_id(client):
-    response = client.get('/emails/1')
-    assert response.status_code == 200
-    assert 'id' in response.json
-    assert 'event_id' in response.json
-    assert 'email_subject' in response.json
-    assert 'email_content' in response.json
-    assert 'timestamp' in response.json
+# def test_get_email_by_id(client):
+#     response = client.get('/save_emails/1')
+#     assert response.status_code == 200
+#     assert 'id' in response.json
+#     assert 'event_id' in response.json
+#     assert 'email_subject' in response.json
+#     assert 'email_content' in response.json
+#     assert 'timestamp' in response.json
 
-def test_delete_email(client):
-    response = client.delete('/emails/1')
-    assert response.status_code == 200
-    assert 'message' in response.json
+# def test_get_email_by_invalid_id(client):
+#     response = client.get('/save_emails/999')
+#     assert response.status_code == 404
+
+# def test_delete_email(client):
+#     response = client.delete('/save_emails/1')
+#     assert response.status_code == 200
+#     assert 'message' in response.json
+
+# def test_delete_email_by_invalid_id(client):
+#     response = client.delete('/save_emails/999')
+#     assert response.status_code == 404
--- test/test_service.py
+++ test/test_service.py
@@ -1,30 +1,121 @@
-from service.email import save, get_all, get_by_id, delete
-from datetime import datetime, timezone
+import os
+import sys
+import pytest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from service.email import save, get_by_id, get_all
+from database.models import Email, EmailRecipient
+from database import session as db
 
 def test_save_email():
     data = {
-        "event_id": 1,
+        "event_id": 123,
         "email_subject": "Test Subject",
         "email_content": "Test Content",
-        "timestamp": "2023-07-31 15:30:00"
+        "timestamp": "2023-07-31 15:30:00",
+        "recipients": ["recipient1@example.com", "recipient2@example.com"]
     }
 
     email = save(data)
-    assert email is not None
-    assert email.event_id == 1
-    assert email.email_subject == "Test Subject"
-    assert email.email_content == "Test Content"
-    assert email.timestamp == datetime(2023, 7, 31, 15, 30, tzinfo=timezone.utc)
+
+    assert email['event_id'] == data['event_id']
+    assert email['email_subject'] == data['email_subject']
+    assert email['email_content'] == data['email_content']
+    assert email['recipients'][0]['recipient_email'] == data['recipients'][0]
+    assert email['recipients'][1]['recipient_email'] == data['recipients'][1]
+
+    # Clean up
+    for recipient in email['recipients']:
+        recipient_id = recipient['id']
+        recipient_obj = db.query(EmailRecipient).get(recipient_id)
+        db.delete(recipient_obj)
+        db.commit()
+
+    email_id = email['id']
+    email_obj = db.query(Email).get(email_id)
+    db.delete(email_obj)
+    db.commit()
+
+def test_get_by_id_email():
+    data = {
+        "event_id": 123,
+        "email_subject": "Test Subject",
+        "email_content": "Test Content",
+        "timestamp": "2023-07-31 15:30:00",
+        "recipients": ["recipient1@example.com", "recipient2@example.com"]
+    }
+
+    email = save(data)
+    email_id = email['id']
+
+    retrieved_email = get_by_id(email_id)
+
+    assert retrieved_email['id'] == email_id
+    assert retrieved_email['event_id'] == data['event_id']
+    assert retrieved_email['email_subject'] == data['email_subject']
+    assert retrieved_email['email_content'] == data['email_content']
+    assert retrieved_email['recipients'][0]['recipient_email'] == data['recipients'][0]
+    assert retrieved_email['recipients'][1]['recipient_email'] == data['recipients'][1]
+
+    # Clean up
+    for recipient in retrieved_email['recipients']:
+        recipient_id = recipient['id']
+        recipient_obj = db.query(EmailRecipient).get(recipient_id)
+        db.delete(recipient_obj)
+        db.commit()
+
+    email_obj = db.query(Email).get(email_id)
+    db.delete(email_obj)
+    db.commit()
 
 def test_get_all_emails():
-    emails = get_all(1, 10)
-    assert 'emails' in emails
-    assert 'pagination' in emails
+    data1 = {
+        "event_id": 123,
+        "email_subject": "Test Subject 1",
+        "email_content": "Test Content 1",
+        "timestamp": "2023-07-31 15:30:00",
+        "recipients": ["recipient1@example.com", "recipient2@example.com"]
+    }
 
-def test_get_email_by_id():
-    email = get_by_id(1)
-    assert email is not None
-    assert email['id'] == 1
+    data2 = {
+        "event_id": 456,
+        "email_subject": "Test Subject 2",
+        "email_content": "Test Content 2",
+        "timestamp": "2023-07-31 16:30:00",
+        "recipients": ["recipient3@example.com", "recipient4@example.com"]
+    }
 
-def test_delete_email():
-    assert delete(1) is True
+    email1 = save(data1)
+    email2 = save(data2)
+
+    emails = get_all(page=1, size=10)
+
+    assert len(emails['data']) >= 2
+    assert emails['pagination']['total_data'] >= 2
+
+    # Clean up
+    for recipient in email1['recipients']:
+        recipient_id = recipient['id']
+        recipient_obj = db.query(EmailRecipient).get(recipient_id)
+        db.delete(recipient_obj)
+        db.commit()
+
+    email_id1 = email1['id']
+    email_obj1 = db.query(Email).get(email_id1)
+    db.delete(email_obj1)
+
+    for recipient in email2['recipients']:
+        recipient_id = recipient['id']
+        recipient_obj = db.query(EmailRecipient).get(recipient_id)
+        db.delete(recipient_obj)
+        db.commit()
+
+    email_id2 = email2['id']
+    email_obj2 = db.query(Email).get(email_id2)
+    db.delete(email_obj2)
+
+    db.commit()
+
+if __name__ == '__main__':
+    pytest.main()
